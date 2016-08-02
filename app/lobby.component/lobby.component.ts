742ed544045c72c54ea8ded509b127aee2fe4747
Jeremy Foster <jeremy.foster@live.com>
2016-08-02 15:03:54 -0700

    add button on lobby to link to race page

--- app/lobby.component/lobby.component.ts
+++ app/lobby.component/lobby.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { Router, ROUTER_DIRECTIVES } from '@angular/router';
+import { MD_BUTTON_DIRECTIVES } from "@angular2-material/button";
 
 @Component({
     moduleId: module.id,
     selector: 'lobby',
     templateUrl: 'lobby.component.html',
     styleUrls: ['lobby.component.css'],
-    directives: [ ROUTER_DIRECTIVES ]
+    directives: [ ROUTER_DIRECTIVES, MD_BUTTON_DIRECTIVES ]
 })
 export class LobbyComponent implements OnInit {
     constructor() {
-        
+
     }
 
-    ngOnInit() { }
+    ngOnInit() { 
+    }
 
+    race() {
+        // this.router.navigate(['/race']);
+    }
 }

412c179aa083a109004dfe6b08850cc46d507817
Jeremy Foster <jeremy.foster@live.com>
2016-08-11 21:12:16 -0700

    add stub for the session visual component

--- app/session.component/session.component.ts
+++ app/session.component/session.component.ts
@@ -1,13 +1,14 @@
 import { Component, provide } from "@angular/core";
 import { MD_TABS_DIRECTIVES } from "@angular2-material/tabs";
 import { SessionTableComponent } from '../session-table.component/session-table.component';
+import { VisualComponent } from '../visual.component/visual.component';
 
 @Component({
     moduleId: module.id,
     selector: "session",
     templateUrl: "session.component.html",
     styleUrls:["session.component.css"],
-    directives: [MD_TABS_DIRECTIVES, SessionTableComponent]
+    directives: [MD_TABS_DIRECTIVES, SessionTableComponent, VisualComponent]
 })
 export class SessionComponent {
     rowers = [
@@ -16,6 +17,7 @@ export class SessionComponent {
         { Name:"Rower 3", Distance:46 }
     ];
     constructor() {
+        
     }
 }
 
---
+++ app/visual.component/visual.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'visual',
+    templateUrl: 'visual.component.html',
+    styleUrls: ['visual.component.css']
+})
+export class VisualComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}

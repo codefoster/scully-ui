898713f239d036a20f72f72ae6d7e0fd9d6bae5b
Jeremy Foster <jeremy.foster@live.com>
2017-08-04 17:36:31 -0700

    replace with cli generated ng4 starter app

--- app/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component/app.component';
-import { appRouterProviders } from './app.routes';
-import { HTTP_PROVIDERS } from '@angular/http';
-
-bootstrap(AppComponent, [
-    appRouterProviders,
-    HTTP_PROVIDERS
-]);

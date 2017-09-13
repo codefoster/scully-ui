31aef42b2293c41df57f197b77de07ed57637185
Jeremy Foster <jeremy.foster@live.com>
2017-09-13 14:41:12 -0700

    formatting changes

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -4,43 +4,43 @@ import { SessionTableComponent } from "../session-table/session-table.component"
 import { VisualComponent } from "../visual/visual.component";
 
 @Component({
-  selector: 'session',
-  templateUrl: './session.component.html',
-  styleUrls: ['./session.component.css'],
-  providers: [ApiService]
+    selector: 'session',
+    templateUrl: './session.component.html',
+    styleUrls: ['./session.component.css'],
+    providers: [ApiService]
 })
 export class SessionComponent implements OnInit {
-  index: number = 0;
-  TAB_COUNT: number = 2;
-  progress: number = 0; 
-  timer;
-  date;
-  countdownindex: number = 3;
+    index: number = 0;
+    TAB_COUNT: number = 2;
+    progress: number = 0;
+    timer;
+    date;
+    countdownindex: number = 3;
 
-  constructor(private api:ApiService) {
-    // Commented out progress bar temporarily.
-    // UPdate the value for the progress-bar on an interval. 
-    // setInterval(() => {
-    //     this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100; 
-    // }, 200);
+    constructor(private api: ApiService) {
+        // Commented out progress bar temporarily.
+        // UPdate the value for the progress-bar on an interval. 
+        // setInterval(() => {
+        //     this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100; 
+        // }, 200);
 
-  }
+    }
 
-  ngOnInit() {
-  }
-    countdownStart(){
+    ngOnInit() {
+    }
+    countdownStart() {
         this.timer = setInterval(() => this.countdown(), 1000);
     }
 
     countdown() {
         if (this.countdownindex === 0) { this.countdownEnd(); }
-        else { 
-            document.getElementById('countdown').innerText = ""+this.countdownindex; 
+        else {
+            document.getElementById('countdown').innerText = "" + this.countdownindex;
             this.countdownindex--;
         }
     }
 
-    countdownEnd(){
+    countdownEnd() {
         clearTimeout(this.timer);
         document.getElementById('countdown').innerText = "";
         this.countdownindex = 3;
@@ -48,14 +48,14 @@ export class SessionComponent implements OnInit {
         this.api.start();
     }
 
-    handleKeypress(event) { 
-        switch(event.key) {
+    handleKeypress(event) {
+        switch (event.key) {
             case "Right":
                 this.index = Math.min(this.index + 1, this.TAB_COUNT - 1);
-                break; 
+                break;
             case "Left":
                 this.index = Math.max(this.index - 1, 0);
-                break; 
+                break;
             case "Space":
                 (this.api.session.status == 'active' ? this.api.session.end() : this.api.session.start())
         }
